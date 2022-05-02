ebe64cb0a78585edbca7696835f66daaaf620c1c
Samrat Man Singh <samrat.singh@vara.ai>
2022-05-02 11:11:06 +0200

    Start over with minimal init.el

--- init.el
+++ init.el
@@ -13,3 +13,24 @@
     (org-babel-load-file (expand-file-name "emacs.org" dotemacs-dir))))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dumb-jump keychain-environment ggtags function-args exec-path-from-shell whitespace-cleanup-mode projectile zenburn-theme markdown-mode color-theme-sanityinc-solarized rg rainbow-delimiters color-theme-sanityinc-tomorrow company auto-complete idle-highlight-mode paredit magit geiser cider smex better-defaults use-package))
+ '(safe-local-variable-values
+   '((nrepl-use-ssh-fallback-for-remote-hosts . t)
+     (cider-ns-refresh-after-fn . "user/start")
+     (cider-ns-refresh-before-fn . "user/stop")
+     (cider-clojure-cli-global-options . "-A:dev:test:package")
+     (eval progn
+	   (put-clojure-indent 'async 1)
+	   (put-clojure-indent 'car/wcar 1)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
